a724acc8c490507ac843c17d5740b8ec52267162
Eugene M. Reyes <eugenemreyes123@gmail.com>
2023-11-10 21:40:19 +0800

    Added commented codes

--- binhi/binhi/settings.py
+++ binhi/binhi/settings.py
@@ -80,6 +80,7 @@ DATABASES = {
     }
 }
 
+# AUTH_USER_MODEL = 'main.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- binhi/main/models.py
+++ binhi/main/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# class CustomUser(AbstractUser):
+  
+#   bio = models.TextField(max_length=500, blank=True, null=True)
+#   first_name = models.CharField(max_length=255, blank=True, null=True,)
+#   last_name = models.CharField(max_length=255, blank=True, null=True,)
+#   is_agree_terms_and_condition = models.BooleanField(default=False)
+
+#   def __str__(self):
+#     return f"{self.first_name} {self.last_name}"
