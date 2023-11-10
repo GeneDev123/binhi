bbade1c9a89e5ea40849ad76a20f4fb65031b543
Eugene M. Reyes <eugenemreyes123@gmail.com>
2023-11-10 22:20:12 +0800

    Added the custom userr setup

--- binhi/main/models.py
+++ binhi/main/models.py
@@ -1,12 +1,12 @@
-# from django.db import models
-# from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# class CustomUser(AbstractUser):
+class CustomUser(AbstractUser):
   
-#   bio = models.TextField(max_length=500, blank=True, null=True)
-#   first_name = models.CharField(max_length=255, blank=True, null=True,)
-#   last_name = models.CharField(max_length=255, blank=True, null=True,)
-#   is_agree_terms_and_condition = models.BooleanField(default=False)
+  bio = models.TextField(max_length=500, blank=True, null=True)
+  first_name = models.CharField(max_length=255, blank=True, null=True,)
+  last_name = models.CharField(max_length=255, blank=True, null=True,)
+  is_agree_terms_and_condition = models.BooleanField(default=False)
 
-#   def __str__(self):
-#     return f"{self.first_name} {self.last_name}"
+  def __str__(self):
+    return f"{self.first_name} {self.last_name}"
