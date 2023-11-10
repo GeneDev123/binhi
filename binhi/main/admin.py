fb47eda4a8d9fc58aadedfff82566588355493e7
Eugene M. Reyes <eugenemreyes123@gmail.com>
2023-11-10 22:25:00 +0800

    Added the admin section for the users

--- binhi/main/admin.py
+++ binhi/main/admin.py
@@ -1,37 +1,37 @@
 from django.contrib import admin
-# from django.contrib.auth.admin import UserAdmin
-# from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# class CustomUserAdmin(UserAdmin):
-#   model = CustomUser
+class CustomUserAdmin(UserAdmin):
+  model = CustomUser
 
-#   list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+  list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
 
-#   fieldsets = (
-#     (None, 
-#       {
-#         'fields': ('username', 'email', 'password')
-#       }),
-#       ('Personal info', 
-#       {
-#         'fields': 
-#           ('first_name', 'last_name', 'bio',)
-#       }),
-#       ('Permissions', 
-#       {
-#         'fields': 
-#           ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
-#       }),
-#       ('Important dates', 
-#       {
-#         'fields': 
-#           ('last_login', 'date_joined')
-#       }),
-#       ('Agreement', 
-#       {
-#         'fields': 
-#           ('is_agree_terms_and_condition',)
-#       }),
-#   )
+  fieldsets = (
+    (None, 
+      {
+        'fields': ('username', 'email', 'password')
+      }),
+      ('Personal info', 
+      {
+        'fields': 
+          ('first_name', 'last_name', 'bio',)
+      }),
+      ('Permissions', 
+      {
+        'fields': 
+          ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+      }),
+      ('Important dates', 
+      {
+        'fields': 
+          ('last_login', 'date_joined')
+      }),
+      ('Agreement', 
+      {
+        'fields': 
+          ('is_agree_terms_and_condition',)
+      }),
+  )
 
-# admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
