def7f7fb98e5ffaa0fc32fe6aa1e0e02fa2ab00f
Eugene M. Reyes <eugenemreyes123@gmail.com>
2023-11-10 22:40:40 +0800

    Added another field and edited existing field. This is for testing purposes

--- binhi/main/models.py
+++ binhi/main/models.py
@@ -3,10 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
   
-  bio = models.TextField(max_length=500, blank=True, null=True)
-  first_name = models.CharField(max_length=255, blank=True, null=True,)
-  last_name = models.CharField(max_length=255, blank=True, null=True,)
+  bio = models.TextField(blank=True, null=True)
+  first_name = models.CharField(max_length=100, blank=True, null=True,)
+  last_name = models.CharField(max_length=100, blank=True, null=True,)
   is_agree_terms_and_condition = models.BooleanField(default=False)
+  career = models.CharField(max_length=255, blank=True, null=True,)
 
   def __str__(self):
     return f"{self.first_name} {self.last_name}"
