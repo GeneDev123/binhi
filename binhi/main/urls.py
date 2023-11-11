392ddaa17b4bf1cbff80b79d1aa241ec8eac3093
Eugene M. Reyes <eugenemreyes123@gmail.com>
2023-11-11 22:52:13 +0800

    Added the page needed for the website

--- binhi/main/urls.py
+++ binhi/main/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
   path('accounts/<str:login_or_register_param>/', views.user_login_and_register, name='login-register'),
+  path('vegetable-recommendations/', views.vegetable_recommendations, name='vegetable_recommendations'),
+  path('cultural-management-practices/', views.cultural_management_practices, name='cultural_management_practices'),
+  path('nutrient-management/', views.nutrient_management, name='nutrient_management'),
+  path('roi-analysis/', views.roi_analysis, name='roi_analysis'),
   path('home/', views.home, name='home'),
   path('', views.home, name='home'),
 ]
